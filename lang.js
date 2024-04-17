e7f586e90c2241f531518eea8c0bc524ce6dddbf
Irina <7764871+ilavva@users.noreply.github.com>
2024-04-17 07:13:50 +0300

    init

---
+++ lang.js
@@ -0,0 +1,15 @@
+let dictionary = {
+    'txt_title': {
+        'en': "ClassMate",
+        'he': "קלאסמט",
+        "ar": "كلاسمت"
+    },
+    'txt_slogan': {
+        'en': "School students for school students",
+        'he': "תלמידים למען תלמידים",
+        "ar": "الطلاب للطلاب"
+    }
+
+
+
+}
