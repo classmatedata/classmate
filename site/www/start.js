d6677fe4e972a10366cf0848e22b903ca5cd1268
Irina <7764871+ilavva@users.noreply.github.com>
2024-05-03 15:21:37 +0300

    database

--- site/www/start.js
+++ site/www/start.js
@@ -1,5 +1,39 @@
-function initData() {
+function loadUserData() {
     let user_data = JSON.parse(localStorage.getItem("user_data"));
     document.querySelector("#text_name").innerText = user_data["name"];
 }
-initData();
+function loadCoursesSearchOptions() {
+    let data_options = JSON.parse(localStorage.getItem("data_options"));
+
+
+    let str_html = "";
+    for (let str of Object.keys(data_options)) {
+        str_html += `<option value="${str}">${data_options[key]["name"]}</option>`;;
+    }
+    document.querySelector("#dl_courses").innerHTML = str_html;
+}
+function loadTopicsSearchOptions() {
+
+    str_html = "";
+    for (let [k, v] of Object.entries(data_options["user_gender"])) {
+        str_html += `<div class="cat">
+           <label>
+              <input type="radio" name="gender" value="${k}"><span>${v}</span>
+           </label>
+        </div>`;
+    }
+    document.querySelector("#dl_courses").innerHTML = str_html;
+}
+
+function initSearchCourseData() {
+
+    load_list('./database/courses.json', (data_options) => {
+        //console.log(data_options);
+        localStorage.removeItem("data_courses");
+        localStorage.setItem("data_courses", JSON.stringify(data_options));
+        loadCoursesSearchOptions();
+    });
+}
+
+loadUserData();
+initSearchCourseData();
