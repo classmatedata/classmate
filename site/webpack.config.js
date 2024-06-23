28f34b167d71dc2e41f37e86c56ffc7bb7083414
Irina <7764871+ilavva@users.noreply.github.com>
2024-06-23 12:52:03 +0300

    added firebase package

--- site/webpack.config.js
+++ site/webpack.config.js
@@ -1,10 +1,8 @@
-const path = require('path')
-module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'www'),
-        filename: 'bundle.js'
-    },
-    watch: true
+import { resolve } from 'path'
+export const mode = 'development'
+export const entry = './src/index.js'
+export const output = {
+    path: resolve(__dirname, 'www'),
+    filename: 'bundle.js'
 }
+export const watch = true
