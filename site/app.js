7ca16beaf4b5cfd2a3352db810939b2ff0f897fb
Irina <7764871+ilavva@users.noreply.github.com>
2024-07-01 15:22:24 +0300

    node js login firebase form

--- site/app.js
+++ site/app.js
@@ -1,14 +1,24 @@
+const express = require('express');
+const router = require("./src/app_routers");
+const cookieParser = require('cookie-parser');
+require("dotenv").config();
+const PORT = process.env.PORT || 8080;
+const HOST = process.env.HOST || 'localhost';
 
-import dotenv from 'dotenv'
-import express from 'express';
 
-dotenv.config();
-let app = express();
 
+const app = express();
+
+app.use(express.json())
+app.use(cookieParser())
 app.use(express.static('public'));
-//=================================================
-const server = app.listen(process.env.SERVER_PORT, process.env.HOST, function () {
-    var host = server.address().address
-    var port = server.address().port
-    console.log("My app is listening at http://%s:%s", host, port)
+app.use(router)
+
+console.log(router);
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT} run http://${HOST}:${PORT}`);
 });
