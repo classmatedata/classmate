ea4fee118cb15d755fe712cded99fee54d56aaaa
Irina <7764871+ilavva@users.noreply.github.com>
2024-07-10 10:57:56 +0300

    more langs added to start_config.html form

--- site/public/start_config.js
+++ site/public/start_config.js
@@ -18,29 +18,40 @@ function initData() {
 }
 
 
-
 function load_list(list_url, callback) {
     fetch(list_url)
-        .then((response) => response.json())
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error(`HTTP error, status = ${response.status}`);
+            }
+            return response.json()
+        })
         .then((json) => callback(json));
 }
 function initQuestionsDataOptions() {
 
 
-    load_list('./database/data_options.json', (data_options) => {
+    load_list('./lang', (data) => {
         //console.log(data_options);
-        localStorage.removeItem("data_options");
-        localStorage.setItem("data_options", JSON.stringify(data_options));
-        loadDataOptions();
+        localStorage.removeItem("data_options_lang");
+        localStorage.setItem("data_options_lang", JSON.stringify(data));
+        fillLangs();
+    });
+
+    load_list('./api/lang', (data) => {
+        //console.log(data_options);
+        localStorage.removeItem("data_options_gender");
+        localStorage.setItem("data_options_gender", JSON.stringify(data));
+        fillGender();
     });
 }
 
-function loadDataOptions() {
-    let data_options = JSON.parse(localStorage.getItem("data_options"));
+function fillLangs() {
+    let data_options_lang = JSON.parse(localStorage.getItem("data_options_lang"));
 
     //fill lang query options
     let str_html = "";
-    for (let [k, v] of Object.entries(data_options["s_lang"])) {
+    for (let [k, v] of Object.entries(data_options_lang)) {
         str_html += `<div class="cat">
            <label>
               <input type="checkbox" name="s_lang" value="${k}"><span>${v}</span>
@@ -48,10 +59,13 @@ function loadDataOptions() {
         </div>`;
     }
     document.querySelector("#config_question_s_langs").innerHTML = str_html;
+}
 
+function fillGender() {
     //fill gender query options
+    let data_options_gender = JSON.parse(localStorage.getItem("data_options_gender"));
     str_html = "";
-    for (let [k, v] of Object.entries(data_options["user_gender"])) {
+    for (let [k, v] of Object.entries(data_options_gender)) {
         str_html += `<div class="cat">
            <label>
               <input type="radio" name="gender" value="${k}"><span>${v}</span>
@@ -64,3 +78,5 @@ function loadDataOptions() {
 
 initData();
 initQuestionsDataOptions();
+
+
